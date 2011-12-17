e52bded88cdbc56396571b58cce4aeb51871e84c
Gianluca Tranchedone <g.tranchedone@gmail.com>
2011-12-17 14:47:35 +0100

    Added Header File and Renamed Passcode Classes

---
+++ GTFramework.h
@@ -0,0 +1,14 @@
+//
+//  GTFramework.h
+//  GTFrameworkDemo
+//
+//  Created by Gianluca Tranchedone on 17/12/11.
+//  Copyright (c) 2011 SketchToCode. All rights reserved.
+//
+
+#ifndef GTFrameworkDemo_GTFramework_h
+#define GTFrameworkDemo_GTFramework_h
+
+
+
+#endif
