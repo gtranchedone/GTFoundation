4c4bd2a38b90ed99d4ec657adc8895d79969d5ac
Gianluca Tranchedone <gianluca@cocoabeans.me>
2013-11-01 15:38:56 +0000

    Improving Google Maps helper classes

--- Classes/GTGoogleMapsTools/GTGoogleGeocoder.h
+++ Classes/GTGoogleMapsTools/GTGoogleGeocoder.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Google. All rights reserved.
 //
 
-#ifdef __CORELOCATION__
+//#ifdef __CORELOCATION__
 
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
@@ -14,7 +14,9 @@
 @interface GTGoogleGeocoder : NSObject
 
 + (void)geocodeAddress:(NSString *)address withCompletionBlock:(void (^)(CLLocation *location, NSError *error))completionBlock;
++ (void)reverseGeocodeLocationWithCoordinate:(CLLocationCoordinate2D)coordinate completionBlock:(void (^)(CLPlacemark *placemark, NSError *error))completionBlock;
++ (void)autocompleteAddress:(NSString *)address mapsApiKey:(NSString *)apiKey completionBlock:(void (^)(NSArray *results, NSError *completionBlock))completionBlock;
 
 @end
 
-#endif
+//#endif
--- Classes/GTGoogleMapsTools/GTGoogleGeocoder.m
+++ Classes/GTGoogleMapsTools/GTGoogleGeocoder.m
@@ -6,16 +6,53 @@
 //  Copyright (c) 2013 Google. All rights reserved.
 //
 
-#ifdef __CORELOCATION__
+//#ifdef __CORELOCATION__
 
 #import "GTGoogleGeocoder.h"
 
 @implementation GTGoogleGeocoder
 
++ (void)autocompleteAddress:(NSString *)address mapsApiKey:(NSString *)apiKey completionBlock:(void (^)(NSArray *, NSError *))completionBlock
+{
+    NSString *geocodingBaseUrl = @"http://maps.googleapis.com/maps/api/place/autocomplete/json?";
+    NSString *url = [NSString stringWithFormat:@"%@input=%@&language=en&types=geocode&sensor=false&key=%@", geocodingBaseUrl, address, apiKey];
+    url = [url stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
+    
+    NSURL *queryUrl = [NSURL URLWithString:url];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSError *error = nil;
+        NSArray *results = nil;
+        
+        NSError *networkError = nil;
+        NSData *responseData = [NSData dataWithContentsOfURL:queryUrl options:NSDataReadingMappedIfSafe error:&networkError];
+        
+        if (responseData && !networkError) {
+            NSError *jsonError = nil;
+            NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&jsonError];
+            
+            if (jsonDictionary && !jsonError) {
+                //results = [self placemarkFromResponseDictionary:jsonDictionary];
+            }
+            else {
+                error = jsonError;
+            }
+        }
+        else {
+            error = networkError;
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (completionBlock) {
+                completionBlock(results, error);
+            }
+        });
+    });
+}
+
 + (void)geocodeAddress:(NSString *)address withCompletionBlock:(void (^)(CLLocation *, NSError *))completionBlock
 {
     NSString *geocodingBaseUrl = @"http://maps.googleapis.com/maps/api/geocode/json?";
-    NSString *url = [NSString stringWithFormat:@"%@address=%@&sensor=false", geocodingBaseUrl,address];
+    NSString *url = [NSString stringWithFormat:@"%@address=%@&sensor=false", geocodingBaseUrl, address];
     url = [url stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
     
     NSURL *queryUrl = [NSURL URLWithString:url];
@@ -49,19 +86,64 @@
     });
 }
 
++ (void)reverseGeocodeLocationWithCoordinate:(CLLocationCoordinate2D)coordinate completionBlock:(void (^)(CLPlacemark *, NSError *))completionBlock
+{
+    NSString *geocodingBaseUrl = @"http://maps.googleapis.com/maps/api/geocode/json?";
+    NSString *formattedCoordinateString = [NSString stringWithFormat:@"%f,%f", coordinate.latitude, coordinate.longitude];
+    NSString *url = [NSString stringWithFormat:@"%@latlng=%@&sensor=false", geocodingBaseUrl, formattedCoordinateString];
+    url = [url stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
+    
+    url = [url stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
+    
+    NSURL *queryUrl = [NSURL URLWithString:url];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        CLPlacemark *placemark = nil;
+        NSError *error = nil;
+        
+        NSError *networkError = nil;
+        NSData *responseData = [NSData dataWithContentsOfURL:queryUrl options:NSDataReadingMappedIfSafe error:&networkError];
+        
+        if (responseData && !networkError) {
+            NSError *jsonError = nil;
+            NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&jsonError];
+            
+            if (jsonDictionary && !jsonError) {
+                // TODO
+            }
+            else {
+                error = jsonError;
+            }
+        }
+        else {
+            error = networkError;
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (completionBlock) {
+                completionBlock(placemark, error);
+            }
+        });
+    });
+}
+
 + (CLLocation *)placemarkFromResponseDictionary:(NSDictionary *)dictionary {
     NSArray *results = [dictionary objectForKey:@"results"];
-    NSDictionary *result = [results objectAtIndex:0];
-    
-    NSDictionary *geometry = [result objectForKey:@"geometry"];
-    NSDictionary *location = [geometry objectForKey:@"location"];
-    NSString *lat = [location objectForKey:@"lat"];
-    NSString *lng = [location objectForKey:@"lng"];
-    
-    return [[CLLocation alloc] initWithLatitude:lat.doubleValue longitude:lng.doubleValue];
+    if (results.count) {
+        NSDictionary *result = [results objectAtIndex:0];
+        
+        NSDictionary *geometry = [result objectForKey:@"geometry"];
+        NSDictionary *location = [geometry objectForKey:@"location"];
+        NSString *lat = [location objectForKey:@"lat"];
+        NSString *lng = [location objectForKey:@"lng"];
+        
+        return [[CLLocation alloc] initWithLatitude:lat.doubleValue longitude:lng.doubleValue];
+    }
+    else {
+        return nil;
+    }
 }
 
 @end
 
-#endif
+//#endif
 
